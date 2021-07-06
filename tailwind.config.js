c7c03e2732a766586b4d10f0010d9bef57bd0565
micheartin <micheartin@gmail.com>
2021-07-06 19:39:26 +0200

    Explain why Tailwind purge is disabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  //purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: false, // ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  // purge is disabled to be able to deploy the storybook on GitHub pages
+
   darkMode: 'class',
+
   jit: true,
+
   plugins: [
     require('@tailwindcss/forms'),
     require('@snowind/plugin')
