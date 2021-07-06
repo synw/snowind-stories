31db31c74aad08d2538b9fb5a75f1d3550608127
micheartin <micheartin@gmail.com>
2021-07-06 20:30:31 +0200

    Reformat Javascript, Typescript, HTML

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="m-8">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
     <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + Snowind" />
   </div>
 </template>
@@ -12,8 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
 export default defineComponent({
   name: 'SnowindStories',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
 </script>
-
