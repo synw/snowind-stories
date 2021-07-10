4cf1bbf21c6ccb9b670e325456508e61acd3383f
synw <synwx@protonmail.com>
2021-07-10 18:47:28 +0200

    Remove eslint vue plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,18 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint';
 import typescript2 from "rollup-plugin-typescript2"
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-
-    eslint(),
-
     typescript2({
       check: false,
       tsconfig: path.resolve(__dirname, 'tsconfig.json'),
       clean: true
     }),
-
     vue(),
   ],
-
   resolve: {
     alias: [
       { find: '@/', replacement: '/src/' }
