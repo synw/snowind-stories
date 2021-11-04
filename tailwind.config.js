f29536b5523c2e4f24b3af45bfce6cafeb76c6e7
synw <synwx@protonmail.com>
2021-11-04 12:13:36 +0100

    Whitelist sm block and hidden classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  // purge: false
-  // purge is disabled to be able to deploy the storybook on GitHub pages
-
+  purge: {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', '../docs/**/*.{vue,js,ts,jsx,tsx}'],
+    options: {
+      safelist: ['sm:block', 'sm:hidden'],
+    }
+  },
   darkMode: 'class',
-
   jit: true,
-
   plugins: [
     require('@tailwindcss/forms'),
     require('@snowind/plugin'),
