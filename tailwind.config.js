6bde3a71abd9d0acef700ac373401905a22df519
synw <synwx@protonmail.com>
2022-01-11 18:43:04 +0100

    Add snowind to tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+      './node_modules/@snowind/**/*.{vue,js,ts}',
+    ],
   },
   // for storybook builds
   //purge: false,
